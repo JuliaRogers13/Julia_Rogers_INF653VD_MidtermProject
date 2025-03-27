847edf453f6be843608bc3790e5bc8cd02420d4c
Julia Rogers <jcrogers4@mail.fhsu.edu>
2025-03-26 19:19:04 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,22 +1,31 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $port = '5432';
-    private $db_name = 'quotesdb';
-    private $username = 'postgres';
-    private $password = 'postgres';
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
     public $conn;
+
+    public function __construct() {
+        $this->host = getenv('HOST');
+        $this->port = getenv('PORT');
+        $this->db_name = getenv('DBNAME');
+        $this->username = getenv('USERNAME');
+        $this->password = getenv('PASSWORD');
+    }
 
     public function connect() {
         $this->conn = null;
         $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name}";
+
         try {
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
         }
+
         return $this->conn;
     }
 }
-?>
